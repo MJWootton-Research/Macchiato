921c4126f93fbffe6394c81d7cbfa842462b8840
Mark Wootton <wootton.mj@gmail.com>
2021-03-17 13:35:15 +0000

    Python 3 & UTF-8 notice

--- BondGraphInterface.py
+++ BondGraphInterface.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
 import os
 import sys
 import math
--- mpn_to_dot.py
+++ mpn_to_dot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
 """
 Takes Petri Net from *.mpn file and writes graph in *.dot format
 """
